647fee40960e315d0a90eec1ff3e4d2a5c585979
Paras Jain <2006paras@gmail.com>
2026-08-18 19:29:20 +0530

    Time: 1 ms (85.31%) | Memory: 43 MB (41.00%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,43 +1,29 @@
 class Solution {
     public String decodeString(String s) {
-        Deque<Integer> noStack =new ArrayDeque();
-        Deque<Character> chrStack =new ArrayDeque();
-        StringBuilder ans =new StringBuilder();
-
-        char c;int k,i=0,x=0;
-
-        while(i<s.length()){
-            if(Character.isDigit(s.charAt(i))){
-                while(Character.isDigit(s.charAt(i))){
-                    x=x*10+s.charAt(i)-48;
-                    i++;
+        Deque<Integer> no=new ArrayDeque<>();
+        Deque<StringBuilder> str=new ArrayDeque<>();
+        StringBuilder cur=new StringBuilder();
+        int n=s.length();
+        int num=0;
+        for(char c:s.toCharArray()){
+            if(Character.isDigit(c)){
+                num=num*10+c-'0';
+            }else if(c=='['){
+                no.push(num);
+                num=0;
+                str.push(cur);
+                cur=new StringBuilder();
+            }else if(c==']'){
+                int k=no.pop();
+                StringBuilder prev=str.pop();
+                for(int i=0;i<k;i++){
+                    prev.append(cur);
                 }
-                    noStack.push(x);
-                    x=0;
-                continue;
-            }else if(s.charAt(i)==']'){
-                StringBuilder temp =new StringBuilder();
-                while(chrStack.peek()!='['){
-                    temp.append(chrStack.pop());
-                }
-                chrStack.pop();
-                temp.reverse();
-                k=noStack.pop();
-                while(k!=0){
-                    k--;
-                for(int j=0;j<temp.length();j++){
-                    chrStack.push(temp.charAt(j));
-                }
-                }
-                
+                cur=prev;
             }else{
-                chrStack.push(s.charAt(i));
+                cur.append(c);
             }
-            i++;
         }
-        while(chrStack.peek()!=null){
-            ans.append(chrStack.pollLast());
-        }
-        return ans.toString();
+        return cur.toString();
     }
 }
