c2e609225715e60b0b43ff12f7810d463d900450
Paras Jain <2006paras@gmail.com>
2026-08-19 01:35:29 +0530

    Time: 2 ms (98.17%) | Memory: 43 MB (86.14%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
+        if(s.length()%2!=0)return false;
         Deque<Character> stack=new ArrayDeque<>();
         for(char c:s.toCharArray()){
             if(c=='[')stack.push(']');
