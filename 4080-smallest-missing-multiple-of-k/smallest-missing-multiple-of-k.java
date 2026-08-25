fa10556b2c96a64aaa602341c65b29e257db9cba
Paras Jain <2006paras@gmail.com>
2026-08-25 16:57:20 +0530

    Time: 1 ms (82.00%) | Memory: 45 MB (71.22%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -1,16 +1,15 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        int[] arr=new int[101];
+        int[] arr=new int[101+k];
 
         for(int i:nums){
             arr[i]=1;
         }
 
-        for(int i=k;i<=100;i=i+k){
+        for(int i=k;;i+=k){
             if(arr[i]!=1){
                 return i;
             }
         }
-        return 100+k-(100%k);
     }
 }
