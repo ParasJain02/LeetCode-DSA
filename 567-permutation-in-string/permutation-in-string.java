7abf467a91bc802ee3356d9cbaa129af22c46378
Paras Jain <2006paras@gmail.com>
2026-08-28 00:35:13 +0530

    Time: 4 ms (99.69%) | Memory: 44.1 MB (60.62%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,35 +1,28 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        //optimization
         int n=s2.length(),m=s1.length();
         if(m>n)return false;
-
-         int[] freq=new int[26];
+        //freq count
+        int[] freq=new int[26];
         for(char c: s1.toCharArray()){
             freq[c-'a']++;
         }
-        int count=m;
-
+        //sliding window
         int left=0;
         for(int right=0;right<n;right++){
             char c=s2.charAt(right);
-
-            if(freq[c-'a']>0){
-                count--;
-            }
+           //reduce char freq
             freq[c-'a']--;
-
-            if(right-left+1==m){
-                if(count==0)return true;
-                else{
-                    if(freq[s2.charAt(left)-'a']>=0){
-                        count++;
-                    }
-                    freq[s2.charAt(left)-'a']++;
-                    left++;
-                }
+            //if freq drops below 0 then shrink left
+            while(freq[c-'a']<0){
+                freq[s2.charAt(left)-'a']++;
+                left++;
             }
-
-
+            //if window size is same as length then its valid
+            if(right-left+1==m){
+                return true;
+            }
         }
         return false;
     }
