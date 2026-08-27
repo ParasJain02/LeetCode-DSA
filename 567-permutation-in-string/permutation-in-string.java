dc2fb9f7fceafe0e0f38ff0f20469e3605215475
Paras Jain <2006paras@gmail.com>
2026-08-28 00:30:36 +0530

    Time: 6 ms (87.77%) | Memory: 44.4 MB (32.37%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        int n=s2.length(),m=s1.length();
+        if(m>n)return false;
+
          int[] freq=new int[26];
         for(char c: s1.toCharArray()){
             freq[c-'a']++;
         }
-        int count=s1.length();
+        int count=m;
 
         int left=0;
-        for(int right=0;right<s2.length();right++){
+        for(int right=0;right<n;right++){
             char c=s2.charAt(right);
 
             if(freq[c-'a']>0){
@@ -15,7 +18,7 @@ class Solution {
             }
             freq[c-'a']--;
 
-            if(right-left+1==s1.length()){
+            if(right-left+1==m){
                 if(count==0)return true;
                 else{
                     if(freq[s2.charAt(left)-'a']>=0){
