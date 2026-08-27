41c2bc42058f31435755512a690aa72027f9e980
Paras Jain <2006paras@gmail.com>
2026-08-28 00:41:14 +0530

    Time: 5 ms (98.27%) | Memory: 47 MB (46.06%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,36 +1,31 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int n=s.length();
-        int m=p.length();
-
-        if(m>n){return new ArrayList<>();}
-
-        int[] freq= new int[26];
-        for(char c:p.toCharArray()){
+        
+        //optimization
+        int n=s.length(),m=p.length();
+        List<Integer> res = new ArrayList<>();
+        if(m>n) return res;
+        //freq count
+        int[] freq=new int[26];
+        for(char c: p.toCharArray()){
             freq[c-'a']++;
         }
-
-
-        int left=0,count=m;
-        List<Integer> ans=new ArrayList<>();
-
-        for(int right=0; right<n; right++){
-            if(freq[s.charAt(right)-'a']>0){
-                count--;
-            }
-            freq[s.charAt(right)-'a']--;
-
-            if(right-left+1==m){
-                if(count==0){
-                    ans.add(left);
-                }
+        //sliding window
+        int left=0;
+        for(int right=0;right<n;right++){
+            char c=s.charAt(right);
+           //reduce char freq
+            freq[c-'a']--;
+            //if freq drops below 0 then shrink left
+            while(freq[c-'a']<0){
                 freq[s.charAt(left)-'a']++;
-                if(freq[s.charAt(left)-'a']>0){
-                    count++;
-                }
                 left++;
             }
+            //if window size is same as length then its valid
+            if(right-left+1==m){
+                res.add(left);
+            }
         }
-        return ans;
+        return res;
     }
 }
