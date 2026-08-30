88f3b28f0db06bfa911c88c939e559132aeb17ed
Paras Jain <2006paras@gmail.com>
2026-08-30 22:03:15 +0530

    Time: 1 ms (99.93%) | Memory: 46.5 MB (54.85%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,22 +1,29 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] seenRow = new boolean[9][9];
-        boolean[][] seenColumn = new boolean[9][9];
-        boolean[][] seenBox = new boolean[9][9];
-        int no,n;
+
+        int[] seenRow = new int[9];
+        int[] seenColumn = new int[9];
+        int[] seenBox = new int[9];
+
+        int no,n,bit;
+
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
+
                 if(board[i][j]=='.')continue;
-                else n=board[i][j]-'0';
+                else n=board[i][j]-'1';
 
                 no=(i/3)*3+(j/3);
 
-                if(seenRow[i][n-1]==true)return false;
-                else seenRow[i][n-1]=true;
-                if(seenColumn[j][n-1]==true)return false;
-                else seenColumn[j][n-1]=true;
-                if(seenBox[no][n-1]==true)return false;
-                else seenBox[no][n-1]=true;
+                bit=1<<n;
+
+                if((seenRow[i]&bit)!=0 || (seenColumn[j]&bit)!=0 || (seenBox[no]&bit)!=0){
+                    return false;
+                }
+
+                seenRow[i]|=bit;
+                seenColumn[j]|=bit;
+                seenBox[no]|=bit;
             }
         }
         return true;
