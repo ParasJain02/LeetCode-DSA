2d8b9e6cc850824420565a900cb162d6e2aedf2d
Paras Jain <2006paras@gmail.com>
2026-08-30 21:38:13 +0530

    Time: 1 ms (99.93%) | Memory: 46.3 MB (69.29%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -9,34 +9,7 @@ class Solution {
                 if(board[i][j]=='.')continue;
                 else n=board[i][j]-'0';
 
-                if(!(n<=9 && n>0))return false;
-                if(i<3){
-                    if(j<3){
-                        no=0;
-                    }else if(j<6){
-                        no=1;
-                    }else{
-                        no=2;
-                    }
-                }
-                else if(i<6){
-                    if(j<3){
-                        no=3;
-                    }else if(j<6){
-                        no=4;
-                    }else{
-                        no=5;
-                    }
-                }
-                else{
-                    if(j<3){
-                        no=6;
-                    }else if(j<6){
-                        no=7;
-                    }else{
-                        no=8;
-                    }
-                }
+                no=(i/3)*3+(j/3);
 
                 if(seenRow[i][n-1]==true)return false;
                 else seenRow[i][n-1]=true;
