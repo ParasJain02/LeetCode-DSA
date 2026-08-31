ae1bc4084955e2c341a7157887647e475225f692
Paras Jain <2006paras@gmail.com>
2026-08-31 19:46:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (30.41%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -3,22 +3,15 @@ class Solution {
         
         int x=m-1;
         int y=n-1;
-        for(int i=m+n-1;i>=0;i--){
-            if(x<0){
-                nums1[i]=nums2[y--];
-                continue;
-            }
-            if(y<0){
-                nums1[i]=nums1[x--];
-                continue;
-            }
-            if(nums1[x]>nums2[y]){
-            nums1[i]=nums1[x];
-            x--;
+        int i=m+n-1;
+
+        while(y>=0){           
+            if(x>=0 && nums1[x]>nums2[y]){
+                nums1[i--]=nums1[x--];
             }else{
-            nums1[i]=nums2[y];
-            y--;
+                nums1[i--]=nums2[y--];
             }
         }
+        
     }
 }
