4bf7c95e26f50b95148ff0db4b24b8bd0e7c40b9
Paras Jain <2006paras@gmail.com>
2026-09-02 21:48:22 +0530

    Time: 2 ms (100.00%) | Memory: 46.6 MB (48.59%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -2,15 +2,16 @@ class Solution {
     public int romanToInt(String s) {
         int sum = 0;
         int n = s.length();
+        int curr,prev=0;
 
-        for (int i = 0; i < n; i++) {
-            int curr = getValue(s.charAt(i));
-
-            if (i + 1 < n && curr < getValue(s.charAt(i + 1))) {
+        for (int i = n-1; i >= 0; i--) {
+            curr = getValue(s.charAt(i));
+            if (i + 1 < n && curr < prev) {
                 sum -= curr;
             } else {
                 sum += curr;
             }
+            prev=curr;
         }
         return sum;
     }
